b12db346c6b37702c241f46cbfb5c8730ee4bb4f
Sergey Pushkin <pushkinsv@gmail.com>
2013-07-30 16:43:20 +0800

    support for hexadecimal values

--- hax_scene.c
+++ hax_scene.c
@@ -18,15 +18,26 @@
 int parse_spec(char **pos, void *out, char type);
 int parse_spec(char **pos, void *out, char type)
 {
-#define IS_DIGIT(c) (((c)>='0' && (c)<='9') || (c)=='-' || (c)=='.')
-#define IS_DELIMITER(c) ((c)==' ' || (c)==';' || (c)==':' || (c)=='\r' || (c)=='\n')
 
 	char *tm1=calloc(strlen(*pos)+1,sizeof(char));
 	char *tm2=tm1;
-	if (type=='i' || type=='f')
+
+	char *digits="0123456789.-";
+	char *whitespace=" \t\n\r";
+
+	if (type=='i') digits="0123456789-";
+	if (type=='f') digits="0123456789.-";
+	if (type=='x') digits="0123456789abcdefABCDEFx";
+	if (type=='s') digits="0123456789-.";
+
+	if (type=='x')
 	{
-		while (**pos && !IS_DIGIT(**pos)) (*pos)++;
-		while (**pos && IS_DIGIT(**pos))
+		while (**pos && strstr(*pos,"0x")!=*pos) (*pos)++;
+	};
+	if (type=='i' || type=='f' || type=='x')
+	{
+		while (**pos && !strchr(digits,**pos)) (*pos)++;
+		while (**pos && strchr(digits,**pos))
 		{
 			*tm2=**pos;
 			(*pos)++;
@@ -35,18 +46,19 @@ int parse_spec(char **pos, void *out, char type)
 	};
 	if (type=='s')
 	{
-		while (**pos && IS_DELIMITER(**pos)) (*pos)++;
-		while (**pos && !IS_DIGIT(**pos))
+		while (**pos && strchr(whitespace,**pos)) (*pos)++;
+		while (**pos && !strchr(digits,**pos))
 		{
 			*tm2=**pos;
 			(*pos)++;
 			tm2++;
 		};
-		while (tm2>tm1 && IS_DELIMITER(**pos))
+		while (tm2>tm1 && strchr(whitespace,**pos))
 		{
 			(*pos)--;
 			tm2--;
 		};
+		tm2++;
 	};
 	*tm2=0;
 
@@ -57,6 +69,7 @@ int parse_spec(char **pos, void *out, char type)
 	};
 	if (type=='i') *((int*)out)=atoi(tm1);
 	if (type=='f') *((float*)out)=atof(tm1);
+	if (type=='x') *((unsigned int*)out)=strtoul(tm1,NULL,16);
 	if (type=='s')
 	{
 		*((char**)out)=(char*)calloc(strlen(tm1)+1,sizeof(char));
@@ -64,7 +77,6 @@ int parse_spec(char **pos, void *out, char type)
 	};
 	free(tm1);
 	return 1;
-#undef IS_NUMBER
 }
 
 
@@ -72,16 +84,22 @@ Scene *scene_create(char *spec)
 {
 	Scene *s=NEW(Scene);
 	char *scene_name=NULL;
+	int version=1;
 	int map_size;
 	float cell_size;
+	unsigned int bgcolor=0x121212;
 	char *ss=spec;
 
 	if (!s) error_exit("out of memory");
 
+	if (!parse_spec(&ss,&version,'i')) error_exit("error reading scene specificatin");
+	printf("version %d\n",version);
 	if (!parse_spec(&ss,&map_size,'i')) error_exit("error reading scene specificatin");
 	printf("map_size %d\n",map_size);
 	if (!parse_spec(&ss,&cell_size,'f')) error_exit("error reading scene specificatin");
 	printf("cell_size %f\n",cell_size);
+	if (!parse_spec(&ss,&bgcolor,'x')) error_exit("error reading scene specificatin");
+	printf("bgcolor 0x%x\n",bgcolor);
 	parse_spec(&ss,&scene_name,'s');
 	if (scene_name) printf("scene_name %s\n",scene_name);
 
