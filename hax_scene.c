d1a02214e909ab8698804880d88b82d4647d2c26
Sergey Pushkin <pushkinsv@gmail.com>
2013-07-30 15:21:55 +0800

    scene specification string parser

--- hax_scene.c
+++ hax_scene.c
@@ -9,20 +9,87 @@
 #endif
 
 #include <stdlib.h>
+#include <string.h>
 #include "hax_scene.h"
 #include "hax_map.h"
 #include "hax_camera.h"
 #include "hax_grid.h"
 
+int parse_spec(char **pos, void *out, char type);
+int parse_spec(char **pos, void *out, char type)
+{
+#define IS_DIGIT(c) (((c)>='0' && (c)<='9') || (c)=='-' || (c)=='.')
+#define IS_DELIMITER(c) ((c)==' ' || (c)==';' || (c)==':' || (c)=='\r' || (c)=='\n')
+
+	char *tm1=calloc(strlen(*pos)+1,sizeof(char));
+	char *tm2=tm1;
+	if (type=='i' || type=='f')
+	{
+		while (**pos && !IS_DIGIT(**pos)) (*pos)++;
+		while (**pos && IS_DIGIT(**pos))
+		{
+			*tm2=**pos;
+			(*pos)++;
+			tm2++;
+		};
+	};
+	if (type=='s')
+	{
+		while (**pos && IS_DELIMITER(**pos)) (*pos)++;
+		while (**pos && !IS_DIGIT(**pos))
+		{
+			*tm2=**pos;
+			(*pos)++;
+			tm2++;
+		};
+		while (tm2>tm1 && IS_DELIMITER(**pos))
+		{
+			(*pos)--;
+			tm2--;
+		};
+	};
+	*tm2=0;
+
+	if (*tm1==0)
+	{
+		free(tm1);
+		return 0;
+	};
+	if (type=='i') *((int*)out)=atoi(tm1);
+	if (type=='f') *((float*)out)=atof(tm1);
+	if (type=='s')
+	{
+		*((char**)out)=(char*)calloc(strlen(tm1)+1,sizeof(char));
+		strcpy(*((char**)out),tm1);
+	};
+	free(tm1);
+	return 1;
+#undef IS_NUMBER
+}
+
+
 Scene *scene_create(char *spec)
 {
 	Scene *s=NEW(Scene);
+	char *scene_name=NULL;
+	int map_size;
+	float cell_size;
+	char *ss=spec;
+
 	if (!s) error_exit("out of memory");
 
-	s->map=map_create();
-	map_create_hex(s->map,4);
+	if (!parse_spec(&ss,&map_size,'i')) error_exit("error reading scene specificatin");
+	printf("map_size %d\n",map_size);
+	if (!parse_spec(&ss,&cell_size,'f')) error_exit("error reading scene specificatin");
+	printf("cell_size %f\n",cell_size);
+	parse_spec(&ss,&scene_name,'s');
+	if (scene_name) printf("scene_name %s\n",scene_name);
 
-	s->grid=grid_create(s->map,0.1f);
+	s->map=map_create();
+	map_create_hex(s->map,map_size);
+
+	s->grid=grid_create(s->map,cell_size);
+	free(scene_name);
 
 	return s;
 }
